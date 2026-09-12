46b217b1c7f1b264ffffd3a9f3fc19509b7c9b95
standardloop <joshuaeli1234@gmail.com>
2026-09-13 00:41:52 +0800

    good progress

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <standardloop/logger.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,7 +7,8 @@
 
 int main(void)
 {
-
+    InitLogger(TRACE, STANDARD_FMT, false, true, true, true);
+    Todo();
     assert(1);
     return EXIT_SUCCESS;
 }
